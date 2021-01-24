9ab8d93806b8a9d2488e1a588103856467fcf26e
Noah Salvi <noah.salvi@ti8m.ch>
2021-01-25 00:11:49 +0100

    make columns have a fixed smallest column width as fallback

--- src/components/blocks/Column.svelte
+++ src/components/blocks/Column.svelte
@@ -2,9 +2,24 @@
   import type BlockColumn from "../../models/blocks/column";
 
   export let block: BlockColumn;
+
+  const smallestColumnSize = 250;
+
+  let clientWidth: number;
+  let minWidth = "min-content";
+
+  $: if (clientWidth <= smallestColumnSize) {
+    minWidth = smallestColumnSize + "px";
+  }
+
+  $: columnWidthPercentage = block.format.column_ratio * 100;
 </script>
 
-<div class="notion-column" style="width: {block.format.column_ratio * 100}%">
+<div
+  class="notion-column"
+  style="width: {columnWidthPercentage}%; min-width: {minWidth}"
+  bind:clientWidth
+>
   <slot />
 </div>
 
@@ -15,6 +30,5 @@
     height: 100%;
     flex-grow: 1;
     flex-shrink: 1;
-    min-width: min-content;
   }
 </style>
